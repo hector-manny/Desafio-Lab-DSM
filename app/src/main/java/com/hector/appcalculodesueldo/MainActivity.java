51ef2958db020cca16b58fec48257e1eea0a13cf
RicardoM99160 <inuriqui_cool2@outlook.com>
2021-02-21 20:20:34 -0600

    Parcial terminado

--- app/src/main/java/com/hector/appcalculodesueldo/MainActivity.java
+++ app/src/main/java/com/hector/appcalculodesueldo/MainActivity.java
@@ -1,48 +1,78 @@
 package com.hector.appcalculodesueldo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     EditText txtNombre;
     EditText txtSalario;
     EditText txtTiempoTrabajado;
-    Button enviar;
 
     String nombre;
     String salario;
     String tiempoTrabajado;
 
+    Toast notificacion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtNombre = findViewById(R.id.);
-        txtSalario = findViewById(R.id.);
-        txtTiempoTrabajado = findViewById(R.id.);
-        enviar = findViewById(R.id.btnEnviar);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtSalario = findViewById(R.id.txtSalario);
+        txtTiempoTrabajado = findViewById(R.id.txtTiempoTrabajado);
     }
 
-    public void enviarDatos() {
+    public void calcularSalario(View v) {
         nombre = txtNombre.getText().toString();
         salario = txtSalario.getText().toString();
         tiempoTrabajado = txtTiempoTrabajado.getText().toString();
 
+        float valorSalario;
+        float valorTiempoTrabajado;
+
+        //Validando que todos los campos se encuentren llenados
         if (validarCampoLlenado(nombre) && validarCampoLlenado(salario) && validarCampoLlenado(tiempoTrabajado)) {
-            if ()
+
+            //Validando que los campos para el salario y el tiempo de antiguedad contengan un valor numerico
+            if (!isNumeric(salario) || !isNumeric(tiempoTrabajado)){
+                notificacion = Toast.makeText(this,"El salario y tiempo trabajdo deben ser valores numericos",Toast.LENGTH_LONG);
+                notificacion.show();
+            }else{
+
+                valorSalario = Float.parseFloat(salario);
+                valorTiempoTrabajado = Float.parseFloat(tiempoTrabajado);
+
+                //Validando que el salario sea mayor o igual que 100 y el tiempo trabajado no sea negativo
+                if(valorSalario >= 100 && valorTiempoTrabajado >= 0){
+                    Intent i = new Intent(this,resultados.class);
+                    i.putExtra("nombre", nombre);
+                    i.putExtra("salario", salario);
+                    i.putExtra("tiempoTrabajado",tiempoTrabajado);
+                    startActivity(i);
+                    limpiarCampos();
+                }else{
+                    notificacion = Toast.makeText(this,"El salario debe ser mayor o igual que 100 y el tiempo de antiguedad mayor o igual que 0",Toast.LENGTH_LONG);
+                    notificacion.show();
+                }
+            }
+        }else{
+            notificacion = Toast.makeText(this,"Todos los campos deben ser completados",Toast.LENGTH_LONG);
+            notificacion.show();
         }
 
     }
 
+    //Funcion para validar que los campos no se encuentren vacíos
     public static boolean validarCampoLlenado(String valor) {
 
-        if (valor == "" || valor == " ") {
+        if (valor.equals("") || valor.equals(" ")) {
             return false;
         } else {
             return true;
@@ -50,16 +80,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public static boolean isNumeric(String string) {
-        boolean result;
-
+    //Función para validar si un valor es de tipo numero
+    private static boolean isNumeric(String cadena){
         try {
-            Double.parseDouble(string);
-            result = true;
-        } catch (NumberFormatException e) {
-            result = false;
+            Float.parseFloat(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
         }
+    }
 
-        return result;
+    private void limpiarCampos(){
+        txtNombre.setText("");
+        txtSalario.setText("");
+        txtTiempoTrabajado.setText("");
     }
 }
--- app/src/main/java/com/hector/appcalculodesueldo/resultados.java
+++ app/src/main/java/com/hector/appcalculodesueldo/resultados.java
@@ -2,13 +2,66 @@ package com.hector.appcalculodesueldo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class resultados extends AppCompatActivity {
+
+    TextView txtvNombre;
+    TextView txtvTiempoTrabajado;
+    TextView txtvSalario;
+    TextView txtvAumento;
+    TextView txtvSalarioNeto;
+
+    float salario;
+    double salarioNeto;
+    float tiempoTrabajado;
+    String aumento;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultados);
+
+        txtvNombre = (TextView) findViewById(R.id.txtvNombre);
+        txtvSalario = (TextView) findViewById(R.id.txtvSalario);
+        txtvTiempoTrabajado = (TextView) findViewById(R.id.txtvTiempoTrabajado);
+        txtvAumento = (TextView) findViewById(R.id.txtvAumento);
+        txtvSalarioNeto = (TextView) findViewById(R.id.txtvSalarioNeto);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String pasoSalario = bundle.getString("salario");
+        String pasoTiempoTrabajado = bundle.getString("tiempoTrabajado");
+        salario = Float.parseFloat(pasoSalario);
+        tiempoTrabajado = Float.parseFloat(bundle.getString("tiempoTrabajado").toString());
+
+        if(salario < 500 && tiempoTrabajado >= 10){
+            aumento = "20%";
+            salarioNeto = (Double) (salario*1.20);
+        }else if(salario < 500 && tiempoTrabajado < 10){
+            aumento = "5%";
+            salarioNeto = (Double) (salario*1.05);
+        }else if(salario >= 500){
+            aumento = "0%";
+            salarioNeto = salario;
+        }
+
+        txtvNombre.setText("Nombre del Empleado: "+bundle.getString("nombre"));
+        txtvSalario.setText("Salario del Empleado: "+salario);
+        txtvTiempoTrabajado.setText("Años de Antiguedad: "+tiempoTrabajado);
+        txtvAumento.setText("Aumento: "+aumento);
+        txtvSalarioNeto.setText("Salario Neto: "+salarioNeto);
+
     }
+
+    public void regresar(View v){
+        finish();
+    }
+
+
 }
